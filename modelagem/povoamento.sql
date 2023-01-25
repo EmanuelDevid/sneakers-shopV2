80d326ee1edda001f094db5786e10212f346ebaf
EmanueDevid <emanueldavid150@gmail.com>
2023-01-25 15:21:03 -0300

    povoando a tabela tenis

---
+++ modelagem/povoamento.sql
@@ -0,0 +1,8 @@
+INSERT INTO tenis(nome, preco, tamanho, descricao, qtd_estoque) VALUES
+("Yeezy Boost 350 V2", "1200.97","40","Air Jordan 5 Retro ‘Crimson Bliss’","100"),
+("Air Jordan 5 Retro","1450.97","41","Air Jordan 5 Retro ‘Crimson Bliss’","423"),
+("Air Jordan 1 Mid SE", "1320.97","38","Air Jordan 5 Retro ‘Crimson Bliss’","154"),
+("Yeezy Boost 700", "1200.97","39","Air Jordan 5 Retro ‘Crimson Bliss’","58"),
+("Nike Wmns Dunk Low", "1620.99","42","Air Jordan 5 Retro ‘Crimson Bliss’","41"),
+("Nike General Purpose Shoe", "1450.97","37","Air Jordan 5 Retro ‘Crimson Bliss’","99");
+SELECT * FROM tenis;
